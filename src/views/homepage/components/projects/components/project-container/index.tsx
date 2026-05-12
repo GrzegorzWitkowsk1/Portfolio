f25d869719724c1d53b59dceb8f298aaa710910a
Grzegorz <grzegorz@motorro.pl>
2026-05-12 11:35:43 +0200

    FEAT: Added position to projects

--- src/views/homepage/components/projects/components/project-container/index.tsx
+++ src/views/homepage/components/projects/components/project-container/index.tsx
@@ -2,14 +2,14 @@ import { useInView } from "react-intersection-observer";
 import { GitHub, OpenInNew } from "@mui/icons-material";
 import { useTranslation } from "react-i18next";
 import {
-	Box,
-	Card,
-	CardMedia,
-	Fade,
-	styled,
-	Tooltip,
-	Typography,
-	useTheme,
+  Box,
+  Card,
+  CardMedia,
+  Fade,
+  styled,
+  Tooltip,
+  Typography,
+  useTheme,
 } from "@mui/material";
 
 import { ContainedStyledButton } from "components/contained-button";
@@ -18,129 +18,138 @@ import { ProjectType } from "../../types";
 import i18n from "locales";
 
 interface Props {
-	project: ProjectType;
-	index: number;
+  project: ProjectType;
+  index: number;
 }
 
 const StyledCard = styled(Card)(({ theme }) => ({
-	borderRadius: "8px",
-	display: "flex",
-	flexDirection: "column",
-	minHeight: "300px",
-	flex: "1 1 calc(50% - 16px)",
-	backgroundColor: "#131316",
-	border: `1px solid ${theme.palette.grey[800]}`,
-	"&:hover": {
-		cursor: "pointer",
-		boxShadow: `
+  borderRadius: "8px",
+  display: "flex",
+  flexDirection: "column",
+  minHeight: "300px",
+  flex: "1 1 calc(50% - 16px)",
+  backgroundColor: "#131316",
+  border: `1px solid ${theme.palette.grey[800]}`,
+  "&:hover": {
+    cursor: "pointer",
+    boxShadow: `
     0 0 9px ${theme.palette.primary.dark},
     0 0 12px  ${theme.palette.primary.dark},
     0 0 15px  ${theme.palette.primary.dark}
   `,
-		border: `1px solid ${theme.palette.primary.main}`,
-	},
+    border: `1px solid ${theme.palette.primary.main}`,
+  },
 }));
 
 const TechnologyBox = styled(Box)(({ theme }) => ({
-	borderRadius: "12px",
-	border: `1px solid ${theme.palette.primary.main}`,
-	backgroundColor: theme.palette.primary.light,
-	color: theme.palette.primary.main,
-	padding: "2px 4px",
-	fontSize: "14px",
+  borderRadius: "12px",
+  border: `1px solid ${theme.palette.primary.main}`,
+  backgroundColor: theme.palette.primary.light,
+  color: theme.palette.primary.main,
+  padding: "2px 4px",
+  fontSize: "14px",
 }));
 
 export default function ProjectContainer(props: Props) {
-	const theme = useTheme();
-	const { t } = useTranslation();
-	const { ref: inViewRef, inView } = useInView({
-		threshold: 0.3,
-		triggerOnce: true,
-	});
+  const theme = useTheme();
+  const { t } = useTranslation();
+  const { ref: inViewRef, inView } = useInView({
+    threshold: 0.3,
+    triggerOnce: true,
+  });
 
-	return (
-		<Fade in={inView} timeout={1000 * (props.index + 1)}>
-			<StyledCard ref={inViewRef}>
-				<CardMedia
-					sx={{ height: "200px", backgroundColor: "white" }}
-					image={props.project.image}
-				/>
-				<Box
-					sx={{
-						display: "flex",
-						flexDirection: "column",
-						height: "100%",
-						justifyContent: "space-around",
-						p: "16px",
-						gap: "4px",
-					}}
-				>
-					<Typography variant="h5" color="white">
-						{props.project.title}
-					</Typography>
-					<Typography variant="body1" color={theme.palette.grey[600]}>
-						{props.project.description[i18n.language]}
-					</Typography>
-					<Box
-						sx={{
-							display: "flex",
-							flexDirection: "row",
-							flexWrap: "wrap",
-							gap: "10px",
-							alignItems: "center",
-						}}
-					>
-						{props.project.technologies.map((t) => (
-							<TechnologyBox key={t}>{t}</TechnologyBox>
-						))}
-					</Box>
-					<Box
-						sx={{
-							display: "flex",
-							gap: "10px",
-							mt: "8px",
-							alignItems: "center",
-						}}
-					>
-						<Tooltip
-							disableHoverListener={!!props.project.codeUrl}
-							title={t("projects.unavailable")}
-						>
-							<div>
-								<OutlinedStyledButton
-									sx={{
-										display: "flex",
-										gap: "8px",
-										alignItems: "center",
-									}}
-									disabled={!props.project.codeUrl}
-								>
-									<GitHub />
-									<Typography>{t("projects.code")}</Typography>
-								</OutlinedStyledButton>
-							</div>
-						</Tooltip>
-						<Tooltip
-							disableHoverListener={!!props.project.demoUrl}
-							title={t("projects.unavailable")}
-						>
-							<div>
-								<ContainedStyledButton
-									sx={{
-										display: "flex",
-										gap: "8px",
-										alignItems: "center",
-									}}
-									disabled={!props.project.demoUrl}
-								>
-									<OpenInNew />
-									<Typography>{t("projects.live")}</Typography>
-								</ContainedStyledButton>
-							</div>
-						</Tooltip>
-					</Box>
-				</Box>
-			</StyledCard>
-		</Fade>
-	);
+  return (
+    <Fade in={inView} timeout={1000 * (props.index + 1)}>
+      <StyledCard ref={inViewRef}>
+        <CardMedia
+          sx={{ height: "200px", backgroundColor: "white" }}
+          image={props.project.image}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            height: "100%",
+            justifyContent: "space-around",
+            p: "16px",
+            gap: "4px",
+          }}
+        >
+          <Typography variant="h5" color="white">
+            {props.project.title}
+          </Typography>
+          <Typography variant="body1" color={theme.palette.grey[600]}>
+            {props.project.description[i18n.language]}
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              gap: "10px",
+              alignItems: "center",
+            }}
+          >
+            {props.project.technologies.map((t) => (
+              <TechnologyBox key={t}>{t}</TechnologyBox>
+            ))}
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              gap: "10px",
+              mt: "8px",
+              alignItems: "center",
+            }}
+          >
+            <Tooltip
+              disableHoverListener={!!props.project.codeUrl}
+              title={t("projects.unavailable")}
+            >
+              <div>
+                <OutlinedStyledButton
+                  sx={{
+                    display: "flex",
+                    gap: "8px",
+                    alignItems: "center",
+                  }}
+                  disabled={!props.project.codeUrl}
+                >
+                  <GitHub />
+                  <Typography>{t("projects.code")}</Typography>
+                </OutlinedStyledButton>
+              </div>
+            </Tooltip>
+            <Tooltip
+              disableHoverListener={!!props.project.demoUrl}
+              title={t("projects.unavailable")}
+            >
+              <div>
+                <a
+                  style={{
+                    textDecoration: "none",
+                  }}
+                  href={props.project.demoUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <ContainedStyledButton
+                    sx={{
+                      display: "flex",
+                      gap: "8px",
+                      alignItems: "center",
+                    }}
+                    disabled={!props.project.demoUrl}
+                  >
+                    <OpenInNew />
+                    <Typography>{t("projects.live")}</Typography>
+                  </ContainedStyledButton>
+                </a>
+              </div>
+            </Tooltip>
+          </Box>
+        </Box>
+      </StyledCard>
+    </Fade>
+  );
 }
--- src/views/homepage/components/projects/types.ts
+++ src/views/homepage/components/projects/types.ts
@@ -1,11 +1,10 @@
 export type ProjectType = {
-	title: string;
-	description: {
-		[key: string]: string;
-	};
-	technologies: string[];
-	codeUrl?: string;
-	image?: string;
-	liveDemoUrl?: string;
-	demoUrl?: string;
+  title: string;
+  description: {
+    [key: string]: string;
+  };
+  technologies: string[];
+  codeUrl?: string;
+  image?: string;
+  demoUrl?: string;
 };
